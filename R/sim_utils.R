50293fb4cf80d27349223e597f1117196dfc7b8c
shafayetShafee <sshafee@isrt.ac.bd>
2023-08-01 17:58:26 +0600

    added utils function safe_and_quietly

---
+++ R/sim_utils.R
@@ -0,0 +1,28 @@
+# combined function of purrr::safely() and purrr::quietly() from
+# https://github.com/tidyverse/purrr/issues/843 by @Maximilian-Stefan-Ernst
+
+safe_and_quietly <- function(fun, ...){
+  safe_fun <- purrr::quietly(purrr::safely(fun))
+  out_safe <- safe_fun(...)
+  
+  length_zero_to_na <- function(obj){
+    if(length(obj)==0){
+      return(NA)
+    }else{
+      return(obj)
+    }
+  }
+  
+  out <- 
+    list(
+      result = out_safe$result$result,
+      error = out_safe$result$error,
+      output = out_safe$output,
+      warnings = out_safe$warnings,
+      messages = out_safe$messages)
+  if(!is.null(out$error)){
+    out$error <- conditionMessage(out$error)
+  }
+  out <- map(out, length_zero_to_na)
+  return(out)
+}
