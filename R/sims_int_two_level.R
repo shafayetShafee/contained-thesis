2bb1cd49eee6d63ac2995e562ca4294a55be34b2
shafayetShafee <sshafee@isrt.ac.bd>
2023-08-28 11:11:45 +0600

    ran for m = all, n = 5 for low prev two lvl int model

--- R/sims_int_two_level.R
+++ R/sims_int_two_level.R
@@ -5,27 +5,27 @@ source(here::here("R/run_simulations.R"))
 # MOR
 
 # m = 10, 30, 50, 100
-cluster_numbers <- c(10)
+cluster_numbers <- c(100)
 
 # n = 5, 10, 30, 50
-cluster_size <- c(10)
+cluster_size <- c(5)
 
 cluster_params <- tidyr::expand_grid(cluster_size = cluster_size, 
                               cluster_numbers = cluster_numbers)
 
-fixed_coeff <- c(-5.2, 1.75, 0.67)
+fixed_coeff <- c(-4, 1.75, 0.67)
 sigma_u_sq <- 2.5
 
 # moderate prevalence => -1.85 (30%)
-# low prev => -5.2 (6%)
+# low prev => -4 (10%)
 
-# log_file <- here::here("log/log_two_lvl_int_low_prev.txt")
-# plot_path <- here::here("plots/two-lvl-ran-int/low-prev/")
-# plot_name_prefix <- "two_lvl_low_prev"
+log_file <- here::here("log/log_two_lvl_int_low_prev.txt")
+plot_path <- here::here("plots/two-lvl-ran-int/low-prev/")
+plot_name_prefix <- "two_lvl_low_prev"
 
-log_file <- here::here("test.txt")
-plot_path <- here::here()
-plot_name_prefix <- "test"
+# log_file <- here::here("test.txt")
+# plot_path <- here::here()
+# plot_name_prefix <- "test"
 
 tictoc::tic()
 res <- purrr::map2_dfr(.x = cluster_params$cluster_numbers, 
@@ -34,22 +34,18 @@ res <- purrr::map2_dfr(.x = cluster_params$cluster_numbers,
                               fixed_coeff = fixed_coeff,
                               sigma_u_sq = sigma_u_sq, 
                               simulation_type = "int",
-                              nsims = 100, seed = 1083,
+                              nsims = 1000, seed = 1083,
                               log_file = log_file, append = TRUE,
                               plot_path = plot_path,
-                              plot_name_suffix = plot_name_prefix)
+                              plot_name_suffix = plot_name_prefix,
+                              more_iter = 1500)
             )
 
 tictoc::toc()
+beepr::beep(3)
 
+# c(609.368, 689.718, 1338.552)
 
-179883
-
-est_two_lvl_int_mor(10, 10, fixed_coeff, sigma_u_sq, 179883)
-
-
-# 2 hour 22 mins (high prev)
-# 2 hour 20 mins (low prev)
 
 # final_res_int_low_prev <- res
 final_res_int_low_prev <- dplyr::bind_rows(final_res_int_low_prev, res)
