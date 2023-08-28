22e75bd823e06e8156a3ad1c0bee1c069c247661
shafayetShafee <sshafee@isrt.ac.bd>
2023-08-28 20:04:40 +0000

    ran for m = all, n = 5, 10 for high (30pc) prev two lvl int model

--- R/sims_int_two_level.R
+++ R/sims_int_two_level.R
@@ -56,3 +56,56 @@ save(final_res_int_low_prev,
 
 saveRDS(final_res_int_low_prev, 
         file=here::here("sim-results/rds/sim_res_two_lvl_int_low_prev.rds"))
+
+
+
+# high prev ---------------------------------------------------------------
+
+# m = 10, 30, 50, 100
+cluster_numbers <- c(100)
+
+# n = 5, 10, 30, 50
+cluster_size <- c(10)
+
+cluster_params <- tidyr::expand_grid(cluster_size = cluster_size, 
+                                     cluster_numbers = cluster_numbers)
+
+fixed_coeff <- c(-1.85, 1.75, 0.67)
+sigma_u_sq <- 2.5
+
+# moderate prevalence => -1.85 (30%)
+# low prev => -4.1 (10%)
+
+log_file <- here::here("log/log_two_lvl_int_high_prev.txt")
+plot_path <- here::here("plots/two-lvl-ran-int/high-prev")
+plot_name_prefix <- "two_lvl_high_prev"
+
+
+tictoc::tic()
+res <- purrr::map2_dfr(.x = cluster_params$cluster_numbers, 
+                       .y = cluster_params$cluster_size, 
+                       .f = ~ run_simulations(m = .x, n = .y, 
+                                              fixed_coeff = fixed_coeff,
+                                              sigma_u_sq = sigma_u_sq, 
+                                              simulation_type = "int",
+                                              nsims = 1000, seed = 1083,
+                                              log_file = log_file, append = TRUE,
+                                              plot_path = plot_path,
+                                              plot_name_suffix = plot_name_prefix,
+                                              more_iter = 1500)
+)
+
+tictoc::toc()
+# beepr::beep(3)
+
+# c(383.327, 500, 647.147 )
+
+# final_res_int_high_prev <- res
+final_res_int_high_prev <- dplyr::bind_rows(final_res_int_high_prev, res)
+
+save(final_res_int_high_prev, 
+     file = here::here("sim-results/rdata/sim_res_two_lvl_int_high_prev.RData"))
+
+saveRDS(final_res_int_high_prev, 
+        file=here::here("sim-results/rds/sim_res_two_lvl_int_high_prev.rds"))
+
