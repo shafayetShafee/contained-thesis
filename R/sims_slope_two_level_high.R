779e576b8be4ba34d30c28822f078883d6fb0d47
shafayetShafee <sshafee@isrt.ac.bd>
2023-09-01 05:45:26 +0600

    completed run for high prev two lvl slp model

--- R/sims_slope_two_level_high.R
+++ R/sims_slope_two_level_high.R
@@ -12,7 +12,7 @@ sigma_mat <- matrix(c(sigma2_u1, sigma2_u12, sigma2_u12, sigma2_u2),
 cluster_numbers <- c(100)
 
 # n = 5, 10, 30, 50
-cluster_size <- c(30)
+cluster_size <- c(50)
 
 cluster_params <- tidyr::expand_grid(cluster_size = cluster_size, 
                                      cluster_numbers = cluster_numbers)
@@ -32,7 +32,7 @@ res <- purrr::map2_dfr(.x = cluster_params$cluster_numbers,
                                               log_file = log_file, append = TRUE,
                                               plot_path = plot_path,
                                               plot_name_suffix = plot_name_prefix,
-                                              more_iter = 500)
+                                              more_iter = 50)
 )
 
 tictoc::toc()
