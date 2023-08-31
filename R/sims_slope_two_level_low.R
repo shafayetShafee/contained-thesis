cbc305475cef22b162a6c3d4c9ad75ef5a8c1ef3
shafayetShafee <sshafee@isrt.ac.bd>
2023-08-31 13:54:41 +0000

    ran for m = 10 and n = 30 for low prev two lvl slp model

--- R/sims_slope_two_level_low.R
+++ R/sims_slope_two_level_low.R
@@ -9,10 +9,10 @@ sigma_mat <- matrix(c(sigma2_u1, sigma2_u12, sigma2_u12, sigma2_u2),
                     byrow = TRUE, nrow = 2, ncol = 2)
 
 # m = 10, 30, 50, 100
-cluster_numbers <- c(100)
+cluster_numbers <- c(10)
 
 # n = 5, 10, 30, 50
-cluster_size <- c(10)
+cluster_size <- c(30)
 
 cluster_params <- tidyr::expand_grid(cluster_size = cluster_size, 
                                      cluster_numbers = cluster_numbers)
@@ -32,7 +32,7 @@ res <- purrr::map2_dfr(.x = cluster_params$cluster_numbers,
                                               log_file = log_file, append = TRUE,
                                               plot_path = plot_path,
                                               plot_name_suffix = plot_name_prefix,
-                                              more_iter = 6000)
+                                              more_iter = 1000)
 )
 
 tictoc::toc()
